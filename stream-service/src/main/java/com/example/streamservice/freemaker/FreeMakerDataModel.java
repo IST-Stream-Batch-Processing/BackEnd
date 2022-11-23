d3c1fd8387577c59e936615fc4b0521babc40b17
Martina <871019650@qq.com>
2022-11-23 12:41:36 +0800

    动态生成java代码

---
+++ stream-service/src/main/java/com/example/streamservice/freemaker/FreeMakerDataModel.java
@@ -0,0 +1,69 @@
+package com.example.streamservice.freemaker;
+
+
+import com.example.streamservice.pojo.Data;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FreeMakerDataModel {
+    private static final String TEMPLATE_PATH = "./src/main/resources/templates";
+    private static final String CLASS_PATH = "./src/main/java/com/example/streamservice/pojo";
+
+    public void generate(){
+        List<String> type = new ArrayList<>();
+        type.add("Long");
+        type.add("Long");
+        type.add("Integer");
+        type.add("String");
+        type.add("Long");
+        ArrayList<String> name = new ArrayList<>();
+        name.add("userId");
+        name.add("itemId");
+        name.add("categoryId");
+        name.add("behavior");
+        name.add("timestamp");
+
+        // step1 创建freeMarker配置实例
+        Configuration configuration = new Configuration();
+        Writer out = null;
+        try {
+            // step2 获取模版路径
+            configuration.setDirectoryForTemplateLoading(new File(TEMPLATE_PATH));
+            // step3 创建数据模型
+            Map<String, Object> dataMap = new HashMap<String, Object>();
+            List<Data> dataList = new ArrayList<>();
+            for(int i=0;i<type.size();i++){
+                dataList.add(new Data(type.get(i),name.get(i)));
+            }
+
+            dataMap.put("classPath", "com.example.streamservice.pojo");
+            dataMap.put("className", "DataClass");
+            dataMap.put("dataList", dataList);
+
+            // step4 加载模版文件
+            Template template = configuration.getTemplate("dataModel.ftl");
+            // step5 生成数据
+            File docFile = new File(CLASS_PATH + "\\" + "DataClass.java");
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
+            // step6 输出文件
+            template.process(dataMap, out);
+            System.out.println("DataClass.java 文件创建成功 !");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.flush();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+}
---
+++ stream-service/src/main/resources/templates/dataModel.ftl
@@ -0,0 +1,27 @@
+package ${classPath};
+
+public class ${className?cap_first} {
+<#list dataList as data>
+    private ${data.type} ${data.name};
+</#list>
+
+    public ${className?cap_first}(){
+    }
+
+    public ${className?cap_first}(<#list dataList as data>${data.type}  ${data.name}<#if data_has_next>,</#if></#list>){
+    <#list dataList as data>
+        this.${data.name}=${data.name};
+    </#list>
+    }
+
+<#list dataList as data>
+
+    public void set${data.name?cap_first} (${data.type}  ${data.name}) {
+        this.${data.name}=${data.name};
+    }
+
+    public  ${data.type} get${data.name?cap_first}() {
+        return this.${data.name};
+    }
+</#list>
+}
