7cfe414e811db3345c17b40f2905229234d11d23
Martina <871019650@qq.com>
2022-11-27 13:55:22 +0800

    测试动态编译、执行java代码成功

--- stream-service/src/test/java/com/example/streamservice/StreamServiceApplicationTests.java
+++ stream-service/src/test/java/com/example/streamservice/StreamServiceApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.streamservice;
 
+//import com.example.streamservice.service.HotItemsService;
+import com.example.streamservice.freemaker.FreeMakerDataModel;
+import com.example.streamservice.util.CodeGenerate;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class StreamServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void test() throws MalformedURLException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        FreeMakerDataModel freeMakerDataModel = new FreeMakerDataModel();
+        freeMakerDataModel.generate();
+        CodeGenerate codeGenerate = new CodeGenerate();
+
+        String dataName = "DataClass.java";
+        codeGenerate.javac(dataName);
+        String pathName = "DataClass";
+        Class<?> clazz = codeGenerate.java(pathName);
+        Object object = clazz.newInstance();
+        System.out.println(object);
     }
+
 
 }
