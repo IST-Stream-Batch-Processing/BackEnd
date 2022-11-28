cf64231a0011dd6dcc0e46bf9b62f42bd157bdb6
Martina <871019650@qq.com>
2022-11-28 16:09:43 +0800

    fix：修改冲突

--- common/src/main/java/cn/ist/lowcoding/common/constant/Constant.java
+++ common/src/main/java/cn/ist/lowcoding/common/constant/Constant.java
@@ -2,7 +2,7 @@ package cn.ist.lowcoding.common.constant;
 
 public class Constant {
     public static final String DATA_INFO_CHANNEL = "data-info-channel";
-    public static final String AS_SERVICE_CHANNEL = "as-com.example.streamservice.service-channel";
-    public static final String ORCHESTRATION_SERVICE_CHANNEL = "orchestration-com.example.streamservice.service-channel";
+    public static final String AS_SERVICE_CHANNEL = "as-service-channel";
+    public static final String ORCHESTRATION_SERVICE_CHANNEL = "orchestration-service-channel";
     public static final String FILE_API_PREFIX = "/fileapi/file/";
 }
