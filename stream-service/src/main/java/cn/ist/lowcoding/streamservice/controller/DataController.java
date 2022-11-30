484b37c9d8e86f5c81f20698c700af743e3545ac
HuHaoYuan <1124730278@qq.com>
2022-11-30 11:03:52 +0800

    Fix: fix dataDO

--- stream-service/src/main/java/cn/ist/lowcoding/streamservice/controller/DataController.java
+++ stream-service/src/main/java/cn/ist/lowcoding/streamservice/controller/DataController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DataController {
 
     @PostMapping("/registerData")
-    public void registerData(){
+    public void registerData() {
         //得到DataDO
         //存入数据库
         //用DataDO生成FMDataModel
--- stream-service/src/main/java/cn/ist/lowcoding/streamservice/model/data/DataDO.java
+++ stream-service/src/main/java/cn/ist/lowcoding/streamservice/model/data/DataDO.java
@@ -8,11 +8,21 @@ import java.util.List;
 
 @Data
 public class DataDO {
-    private String dataSourceId;//对应的数据源id
-    private String userId;//对应的用户id
-    private String dataId;//自己的id标识
-    private String className; //设置的数据名称
-    private List<TypeAndName> attributeList = new ArrayList<>();//注册数据类中属性类型与名称
-    private Boolean isTimeStamp;//是否使用时间戳,如果为是，则指定的时间戳类型必须为Long
-    private String timeStampName;//时间戳属性名称
+    private String id = ""; // 数据源 ID
+
+    private String filePath = ""; // 文件路径
+
+    private String userId = ""; // DataSource 所对应的用户 ID
+
+    private List<String> process = new ArrayList<>(); // DataSource 所对应的编排
+
+
+    // 数据源对应的数据
+    private String className; // 设置的数据名称
+
+    private List<TypeAndName> attributeList = new ArrayList<>(); // 注册数据类中属性类型与名称
+
+    private Boolean isTimeStamp; // 是否使用时间戳,如果为是，则指定的时间戳类型必须为Long
+
+    private String timeStampName; // 时间戳属性名称
 }
