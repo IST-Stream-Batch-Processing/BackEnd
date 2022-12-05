8144b63510894b4d01a864becd31b836e30f4c2f
Martina <871019650@qq.com>
2022-12-05 14:15:15 +0800

    feat: aggregate算子

---
+++ stream-service/src/main/java/cn/ist/lowcoding/streamservice/freemaker/FMAggregate.java
@@ -0,0 +1,63 @@
+package cn.ist.lowcoding.streamservice.freemaker;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import lombok.Data;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class FMAggregate {
+    private static final String TEMPLATE_PATH = "./src/main/resources/templates";
+    private static final String CLASS_PATH = "./src/main/java/cn/ist/lowcoding/streamservice/generateClass";
+    private static final String PACKAGE_PATH = "cn.ist.lowcoding.streamservice.generateClass";
+
+    private String operatorId;
+    private String finalType; //固定（直接写入.ftl报错）
+    private String LongType = "Long"; //固定（直接写入.ftl报错）
+    private String originalType;//输入数据流类型(keyby后)
+    private String keyType;//key数据类型
+
+    public void generate() {
+
+//        String finalType = "ItemViewCount";
+//        String LongType = "Long";
+//        String originalType = "DataClass";
+//        String keyType = "Long";
+
+        Configuration configuration = new Configuration();
+        // step3 创建数据模型
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        Writer out = null;
+        try {
+            // step2 获取模版路径
+            configuration.setDirectoryForTemplateLoading(new File(TEMPLATE_PATH));
+
+            Template template = configuration.getTemplate("aggregateModel.ftl");
+            // step5 生成数据
+            File docFile = new File(CLASS_PATH + "\\" + "StreamAggregate"+operatorId+".java");
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
+            dataMap.put("operatorId",operatorId);
+            dataMap.put("packagePath", PACKAGE_PATH);
+            dataMap.put("originalType",originalType);
+            dataMap.put("finalType",finalType);
+            dataMap.put("keyType",keyType);
+            dataMap.put("LongType",LongType);
+            template.process(dataMap, out);
+            System.out.println("StreamAggregate"+operatorId+".java 文件创建成功 !");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.flush();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+}
+
